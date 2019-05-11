fa091320932f7b807622ef69a76def422bcdcee3
joe <rementeriajoseph@gmail.com>
2019-05-11 17:07:24 -0600

    cambios a la entidad viaje

--- script.sql
+++ script.sql
@@ -86,18 +86,19 @@ CREATE TABLE conduce (
 
 CREATE TABLE viaje (
     pk_placa_taxi		        VARCHAR(8),
-    correo_cliente	            VARCHAR(64),
-    pk_fecha_inicio		        TIMESTAMP			NOT NULL,
-    fecha_fin			        TIMESTAMP			NOT NULL,
-    costo				        VARCHAR(8)			NOT NULL,
+    fk_correo_cliente	        VARCHAR(64),
+    pk_fecha_inicio		        TIMESTAMP           NOT NULL,
+    fecha_fin			        TIMESTAMP,
+    costo				        VARCHAR(8),
     estrellas			        FLOAT NULL,
-    origen_destino		        VARCHAR(64)			NOT NULL,
+    origen	        	        VARCHAR(64)			NOT NULL,
+    destino                     VARCHAR(64),
     correo_taxista		        VARCHAR(8)			NOT NULL,
-    agenda_telefono             VARCHAR(8)          NOT NULL,
-    agenda_nombre               VARCHAR(128)        NOT NULL,
-    fk_agenda_correo_operador   VARCHAR(654)        NOT NULL,
+    agenda_telefono             VARCHAR(8),
+    agenda_nombre               VARCHAR(128),
+    fk_agenda_correo_operador   VARCHAR(64),
     
-    CONSTRAINT fk_viaje_cliente FOREIGN KEY (correo_cliente) REFERENCES cliente (pk_correo_usuario) ON DELETE NO ACTION ON UPDATE CASCADE,
+    CONSTRAINT fk_viaje_cliente FOREIGN KEY (fk_correo_cliente) REFERENCES cliente (pk_correo_usuario) ON DELETE NO ACTION ON UPDATE CASCADE,
     CONSTRAINT fk_viaje_taxi FOREIGN KEY (pk_placa_taxi) REFERENCES taxi (pk_placa) ON DELETE NO ACTION ON UPDATE CASCADE,
     CONSTRAINT fk_viaje_taxista FOREIGN KEY (correo_taxista) REFERENCES taxista (pk_correo_usuario) ON DELETE NO ACTION ON UPDATE CASCADE,
     CONSTRAINT fk_viaje_operador FOREIGN KEY (fk_agenda_correo_operador) REFERENCES operador (pk_correo_usuario) ON DELETE NO ACTION ON UPDATE CASCADE,
@@ -149,6 +150,7 @@ CREATE TRIGGER tr_sumar_tiempo_expiracion_token_before_update_e
 BEFORE UPDATE ON token_recuperacion_contrasena
 FOR EACH ROW 
 BEGIN
+		SET new.fecha_expiracion = TIMESTAMPADD(HOUR, 12, CURRENT_TIMESTAMP);
 		SET new.fecha_expiracion = TIMESTAMPADD(HOUR, 12, CURRENT_TIMESTAMP);
 END; //
 DELIMITER ;
